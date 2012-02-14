ec7ed1b0ff7612fc10656644f39bdf9a0f55b087
genzoo <cottefoglie@algorithmica.it>
2012-02-14 23:02:02 +0100

    added lgpl licence header in all source

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,32 @@
+###############################################################################
+###############################################################################
+#
+# This file is part of GeoGraph.
+#
+# Copyright (c) 2012 Algorithmica Srl
+#
+# GeoGraph is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GeoGraph is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with GeoGraph.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact us via email at info@algorithmica.it or at
+#
+# Algorithmica Srl
+# Vicolo di Sant'Agata 16
+# 00153 Rome, Italy
+#
+###############################################################################
+###############################################################################
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
