85a7c47107e7542cf80ae0234d61bdd3a8b8b0a2
ziparo <ziparo@algorithmica.it>
2012-03-23 13:36:52 +0100

    Updated deploy rb with ssh options

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,14 +12,22 @@ set :scm_verbose,       true
 set :use_sudo,          false
 
 # Production server
+
 set :deploy_to,         "/opt/apps/geograph"
 set :torquebox_home,    "/opt/torquebox/current"
 set :jboss_init_script, "/etc/init.d/jboss-as-standalone"
 set :app_environment,   "RAILS_ENV: production"
 set :app_context,       "/"
 
+#Added by vittorio
+#default_run_options[:pty] = true
+#ssh_options[:verbose] = :debug
+ssh_options[:auth_methods] = "publickey"
+ssh_options[:keys] = %w(~/.ssh/id_rsa)
+
+
 ssh_options[:forward_agent] = false
 
-role :web, "vm-102.uc.futuregrid.org"
-role :app, "vm-102.uc.futuregrid.org"
-role :db,  "vm-102.uc.futuregrid.org", :primary => true
+role :web, "vm-103.uc.futuregrid.org"
+role :app, "vm-103.uc.futuregrid.org"
+role :db,  "vm-103.uc.futuregrid.org", :primary => true
