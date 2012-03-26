67bdb898b748b186e8bbb250a727dbbf57f590aa
ziparo <ziparo@algorithmica.it>
2012-03-26 03:05:52 +0200

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,13 +10,13 @@ set :user,              "torquebox"
 set :scm,               :git
 set :scm_verbose,       true
 set :use_sudo,          false
-
+set :test_server,       "vm-102.uc.futuregrid.org"
 # Production server
 
-set :deploy_to,         "/opt/apps/geograph"
+set :deploy_to,         "/opt/apps/#{application}"
 set :torquebox_home,    "/opt/torquebox/current"
 set :jboss_init_script, "/etc/init.d/jboss-as-standalone"
-#set :app_environment,   "RAILS_ENV: production"
+#set :app_environment,   "RAILS_ENV: production" DOES NOT WORK!!!
 set :app_context,       "/"
 
 
@@ -25,13 +25,13 @@ set :app_context,       "/"
 #ssh_options[:verbose] = :debug
 ssh_options[:auth_methods] = "publickey"
 ssh_options[:keys] = %w(~/.ssh/id_rsa)
-
+set :deploy_via, :remote_cache
 
 ssh_options[:forward_agent] = false
 
 #Precompile asset pipeline
 load 'deploy/assets'
 
-role :web, "vm-102.uc.futuregrid.org"
-role :app, "vm-102.uc.futuregrid.org"
-role :db,  "vm-102.uc.futuregrid.org", :primary => true
+role :web, "#{test_server}"
+role :app, "#{test_server}"
+role :db,  "#{test_server}", :primary => true
