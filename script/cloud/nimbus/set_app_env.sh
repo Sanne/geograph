baee656b0e5ad485b434d5af06137a18518e0a2c
ziparo <ziparo@algorithmica.it>
2012-04-13 19:53:04 +0200

    fixed nimbus scripts

--- script/cloud/nimbus/set_app_env.sh
+++ script/cloud/nimbus/set_app_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /etc/jboss-as/jboss-as.conf
+JRUBY_HOME=/opt/torquebox/current/jruby
 GEOGRAPH_HOME=/opt/apps/geograph/current
 AGENT_FARM_HOME=/opt/apps/geograph-agent-farm/current
