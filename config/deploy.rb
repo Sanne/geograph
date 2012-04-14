926d8e18fd76c2af1c111df5b80117bffbda84ed
ziparo <ziparo@algorithmica.it>
2012-04-14 13:08:31 +0200

    fix: removed conflict

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,6 @@ ssh_options[:forward_agent] = false
 load 'deploy/assets'
 
 
-role :web, "vm-117.uc.futuregrid.org"
-role :app, "vm-117.uc.futuregrid.org"
-role :db,  "vm-117.uc.futuregrid.org", :primary => true
+role :web, "vm-103.uc.futuregrid.org"
+role :app, "vm-103.uc.futuregrid.org"
+role :db,  "vm-103.uc.futuregrid.org", :primary => true
